44f91ef6c1aa979c6c8086c848091c8be5a1eaaf
amckean12 <amckean@me.com>
2019-05-23 16:31:52 -0500

    Updated menu container took out for loop

--- src/menu/containers/menu-container.js
+++ src/menu/containers/menu-container.js
@@ -6,12 +6,6 @@ class MenuContainer extends Component {
 
 
   render(){
-    let today = new Date()
-    console.log(today)
-    let maxDay = today.setDate(today.getDate() + 7)
-    console.log(maxDay)
-    let todayParsed = today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate()
-    console.log(todayParsed)
 
     return(
       <div className="menu-container">
