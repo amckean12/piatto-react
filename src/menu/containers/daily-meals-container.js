15d0bb08f9997dcf9f6a9f95f5627be13436c4b7
amckean12 <amckean@me.com>
2019-05-23 16:31:12 -0500

    Began work on client menu portion

---
+++ src/menu/containers/daily-meals-container.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+class DailyMealsContainer extends Component {
+
+
+  render(){
+    return(
+      <div className="daily-meals-container">
+        Hi
+      </div>
+    )
+  }
+}
+
+
+
+export default DailyMealsContainer
--- src/menu/containers/menu-container.js
+++ src/menu/containers/menu-container.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import DailyMealsContainer from './daily-meals-container.js'
 
 class MenuContainer extends Component {
 
 
   render(){
+    let today = new Date()
+    console.log(today)
+    let maxDay = today.setDate(today.getDate() + 7)
+    console.log(maxDay)
+    let todayParsed = today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate()
+    console.log(todayParsed)
+
     return(
       <div className="menu-container">
-        Menu-Container
+        <h1>Menu For Alex</h1>
+
       </div>
     )
   }
