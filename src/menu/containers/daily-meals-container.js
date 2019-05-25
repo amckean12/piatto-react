8df5ef9e727486199593c868d0004b6262bbd479
amckean12 <amckean@me.com>
2019-05-25 10:28:52 -0500

    Added button to expanded meal box component

--- src/menu/containers/daily-meals-container.js
+++ src/menu/containers/daily-meals-container.js
@@ -25,6 +25,10 @@ class DailyMealsContainer extends Component {
     }
   }
 
+  handleMealAdd(event){
+    console.log("this method has been hit")
+  }
+
   addDescriptionComponent(){
     return <MealBoxDescription mealDescription= { this.props.food.description } />
   }
@@ -34,6 +38,7 @@ class DailyMealsContainer extends Component {
       <div className={this.state.mealBoxClassName} onClick={(event) => this.handleClick(event)}>
         <MealBoxTitle recipeName={ this.props.food.name } />
         { this.state.mealBoxClassName === "meal-box open" ? this.addDescriptionComponent() : <p>Click Me</p>}
+        { this.state.mealBoxClassName === "meal-box open" ? <button onClick={(event) => this.handleMealAdd(event)}>Add to Menu</button> : <p>Click Me</p>}
       </div>
     )
   }
