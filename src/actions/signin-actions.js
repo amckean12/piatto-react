87d0a8d082db633aa14b2477af14fa5769ce4401
amckean12 <amckean@me.com>
2019-05-30 08:35:42 -0500

    Quick refactor of actions in code

--- src/actions/signin-actions.js
+++ src/actions/signin-actions.js
@@ -3,20 +3,32 @@ import { getUser } from './get-user.js'
 import { API_URL } from './api_URL'
 import fetch from 'cross-fetch'
 
+
 export const loginUserFetch = credentials => {
+  //Fetch options body:
+  const userCredentials = JSON.stringify({auth: credentials})
+
+  //fetch options for logging in a new user
+  const options = {
+    method: 'post',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: userCredentials
+  }
+
+  const fetchURL = `${API_URL}/user_token`
+
+  //Action to send request to API to get Token
   return dispatch => {
     dispatch(authRequest())
-    return fetch(`${API_URL}/user_token`, {
-      method: "POST",
-      headers:{
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({auth: credentials})
-    })
+    return fetch(fetchURL, options)
     .then(resp => resp.json())
     .then((response) => {
         const token = response.jwt;
+        //For now setting JWT in local storage see this in chrome console by localStorage.token
         localStorage.setItem('token', token);
+        //Calling action to find user in API
         return getUser(credentials)
     })
     .then((user) => {
--- src/actions/signup-actions.js
+++ src/actions/signup-actions.js
@@ -4,9 +4,10 @@ import { API_URL } from './api_URL'
 import fetch from 'cross-fetch'
 
 export const userSignUpFetch = user => {
-  //Fetch request info
+  //Fetch options body
   const newUser = JSON.stringify({user: user})
 
+  // fetch options for signing up user
   const options = {
     method: 'post',
     headers: {
@@ -14,11 +15,14 @@ export const userSignUpFetch = user => {
     },
     body: newUser
   }
+
   const fetchURL = `${API_URL}/users`
 
+  // Action to send request to API to signup user
   return dispatch => {
     return fetch(fetchURL, options)
       .then(resp => resp.json())
+      //calling action to log in user
       .then(jresp => { dispatch(loginUserFetch({
         first_name: user.first_name,
         last_name: user.last_name,
