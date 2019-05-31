420ff869c5a99b5656913cea8e6f19a333514043
amckean12 <amckean@me.com>
2019-05-31 14:15:25 -0500

    Installed redux persist

--- src/profile/containers/navbar-container.js
+++ src/profile/containers/navbar-container.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import NavbarLinkComponent from '../components/navbar-link-component';
 import NavbarProfileComponent from '../components/navbar-profile-component';
 import { getUser } from '../../actions/get-user.js'
+import { Route } from 'react-router-dom'
 
 class NavbarContainer extends Component {
 
@@ -18,7 +19,6 @@ class NavbarContainer extends Component {
       <div className='navbar-container'>
         <NavbarProfileComponent />
         <NavbarLinkComponent value={"First Name"} />
-        <NavbarLinkComponent value={"Menu"} />
         <NavbarLinkComponent value={"Friends"} />
         <NavbarLinkComponent value={"Settings"} />
         <NavbarLinkComponent value={"Logout"} />
---
+++ src/recipe/containers/new-recipe-container.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class NewRecipeContainer extends Component {
+
+  render(){
+    return(
+      <div className='new-recipe-container'>
+      </div>
+    )
+  }
+}
+
+
+export default(NewRecipeContainer
+)
