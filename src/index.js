bca5a86eb3a4355f507133fa4d8d6888c8eee12a
amckean12 <amckean@me.com>
2019-05-31 19:14:51 -0500

    Added comments

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react'
+import { PersistGate } from 'redux-persist/lib/integration/react' //Needed for react configuration
 import { store, persistor } from './store.js'
 import 'babel-polyfill'
 
--- src/store.js
+++ src/store.js
@@ -1,30 +1,33 @@
+//Needed for creation of Redux store
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/root-reducer.js';
 
-//Persistance
+//Needed for persistance
 import storage from 'redux-persist/lib/storage'
 import { persistStore, persistReducer } from 'redux-persist'
 
+//Persistance configuration
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ['userReducer']
 }
 
+//persistReducer takes in a configuration argument and a reducer
 const reducers = persistReducer(persistConfig, rootReducer)
 
-
+//Needed for chrome devtools
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-//export const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
-
+//Store Creation
 const store = createStore(
   reducers,
   undefined,
   composeEnhancer(applyMiddleware(thunk))
 )
 
+//Store Persistor
 const persistor = persistStore(store);
 
 export { store, persistor }
