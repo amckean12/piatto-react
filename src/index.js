25a15a74afd94858e755714f45fed885f43caf10
amckean12 <amckean@me.com>
2019-05-31 19:08:09 -0500

    Added user persistance during hard reloads

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { store } from './store.js'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import { store, persistor } from './store.js'
 import 'babel-polyfill'
 
 
 
 ReactDOM.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'));
 
--- src/store.js
+++ src/store.js
@@ -3,9 +3,28 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers/root-reducer.js';
 
 //Persistance
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ['userReducer']
+}
+
+const reducers = persistReducer(persistConfig, rootReducer)
 
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+//export const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+
+const store = createStore(
+  reducers,
+  undefined,
+  composeEnhancer(applyMiddleware(thunk))
+)
+
+const persistor = persistStore(store);
+
+export { store, persistor }
