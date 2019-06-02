dd3adfb28c7b3f8f9d456818afcfa93185cd57c4
amckean12 <amckean@me.com>
2019-06-02 14:03:45 -0500

    Have front end mark up of new recipe form set up

--- src/recipe/containers/recipe-container.js
+++ src/recipe/containers/recipe-container.js
@@ -5,13 +5,34 @@ import '../recipe.css'
 
 
 class RecipeContainer extends Component {
+  state = {
+    recipeName: "",
+    description: "",
+    ingredients: [],
+    tags: [],
+  }
 
+  handleChange(event){
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
+  handleSubmit(event){
+    event.preventDefault();
+    console.log("hit the submit")
+  }
 
   render(){
     return(
       <div className="recipe-container">
-        <form>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <h1>Create Account</h1>
+          <input type="text" placeholder="Recipe Name" name="recipeName" onChange={(event) => this.handleChange(event)} value={this.state.RecipeName}/>
+          <input type="text" placeholder="Recipe Description" name="description" onChange={(event) => this.handleChange(event)} value={this.state.description}/>
+          <input type="text" placeholder="ingredients" name="ingredients" onChange={(event) => this.handleChange(event)} value={this.state.ingredients}/>
+          <input type="text" placeholder="tags" name="tags" onChange={(event) => this.handleChange(event)} value={this.state.tags}/>
+          <button>Create Recipe</button>
         </form>
       </div>
     )
