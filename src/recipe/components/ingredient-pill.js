90f9481053dad7992011876179bb1a20ecf98bf7
amckean12 <amckean@me.com>
2019-06-07 12:05:36 -0500

    styling of ingredient pill

--- src/recipe/components/ingredient-pill.js
+++ src/recipe/components/ingredient-pill.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 import '../pill.css'
 
 const IngredientPill = (props) => {
   return (
     <div className="ingredient-pill">
-      Hamburger <FontAwesomeIcon icon={ faCoffee }/>
+       <div className="ingredient-tag">
+        Hamburger
+          <button className="ingredient-tag-exit"><FontAwesomeIcon icon= { faTimes }/></button>
+       </div>
     </div>
   )
 }
