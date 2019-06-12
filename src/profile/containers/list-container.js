85782c5a02c59e9adc70322230e6ce136fe36a61
amckean12 <amckean@me.com>
2019-06-12 13:30:46 -0500

    removed nutrition aspect to complicated of scope for now

--- src/profile/containers/list-container.js
+++ src/profile/containers/list-container.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import LinkComponent from '../components/list-container-link.js'
-import MacroTrackerWheel from '../components/macro-tracker-wheel.js'
 import GroceryListComponent from '../components/grocery-list-component.js'
 
 class ListContainer extends Component {
@@ -10,8 +8,6 @@ class ListContainer extends Component {
   render(){
     return(
       <div className="list-container">
-        <LinkComponent linkName={"Nutrition Value"} />
-        <MacroTrackerWheel />
         <GroceryListComponent testPropToPass={"Grocery List"}/>
       </div>
     )
