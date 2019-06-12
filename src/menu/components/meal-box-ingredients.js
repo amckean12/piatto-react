ae3d8cfc9865b84e8d3145b0300a555cbd177c91
amckean12 <amckean@me.com>
2019-06-12 12:54:57 -0500

    Added ingredients and styling to recipe box

--- src/menu/components/meal-box-ingredients.js
+++ src/menu/components/meal-box-ingredients.js
@@ -5,6 +5,12 @@ const MealBoxIngredients = (props) => {
   return (
     <div className="meal-box-ingredients">
       <h2>Ingredients</h2>
+      <ul>
+        {
+          props.ingredients.map(ingredient =>
+          <li>{ingredient.description}</li>)
+        }
+      </ul>
     </div>
   )
 }
