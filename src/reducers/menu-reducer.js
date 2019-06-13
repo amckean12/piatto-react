79376ad9868d5e9ffe1f30412cc1c99572a3a21b
amckean12 <amckean@me.com>
2019-06-13 15:13:24 -0500

    working on state fix for logout

--- src/reducers/menu-reducer.js
+++ src/reducers/menu-reducer.js
@@ -1,3 +1,5 @@
+import * as types from '../actions/action-types';
+
 const initialMenuState = {
   recipes: [],
   ingredients: [],
@@ -7,8 +9,12 @@ export default function reducer(state = initialMenuState, action) {
     switch (action.type) {
       case 'ADD_RECIPE_TO_MENU':
         return {...state,
-                recipes: [...state.recipes, action.recipe],
-                ingredients: [...state.ingredients, action.recipe.ingredients]}
+                recipes: [...state.recipes,
+                          action.recipe],
+                ingredients: [...state.ingredients,
+                              action.recipe.ingredients]}
+      case types.LOGOUT:
+        return{state: initialMenuState}
       default:
         return state;
     }
--- src/reducers/recipe-reducer.js
+++ src/reducers/recipe-reducer.js
@@ -1,3 +1,5 @@
+import * as types from '../actions/action-types';
+
 const initialRecipeState = {
   recipeName: "",
   recipeDescription: "",
