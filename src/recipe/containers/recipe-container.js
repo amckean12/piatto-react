15033e9cfc20174219f74248ea695aa43bae651c
amckean12 <amckean@me.com>
2019-06-15 15:56:43 -0500

    Cleaning up unecessary code for project review

--- src/recipe/containers/recipe-container.js
+++ src/recipe/containers/recipe-container.js
@@ -14,29 +14,9 @@ class RecipeContainer extends Component {
     tags: [],
   }
 
-  handleChange(event){
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-
-  handleSubmit(event){
-    event.preventDefault();
-    console.log("hit the submit")
-  }
-
   render(){
     return(
       <div className="form-container recipe-container">
-        <form onSubmit={(event) => this.handleSubmit(event)}>
-          <h1>Create Recipe</h1>
-          <input type="text" placeholder="Recipe Name" name="recipeName" onChange={(event) => this.handleChange(event)} value={this.state.RecipeName}/>
-          <input type="text" placeholder="Recipe Description" name="description" onChange={(event) => this.handleChange(event)} value={this.state.description}/>
-          <input type="text" placeholder="ingredients" name="ingredients" onChange={(event) => this.handleChange(event)} value={this.state.ingredients}/>
-          <input type="text" placeholder="tags" name="tags" onChange={(event) => this.handleChange(event)} value={this.state.tags}/>
-          <button>Create Recipe</button>
-        </form>
       </div>
     )
   }
