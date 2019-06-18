0d1eab639bd4e74d43508ffad1e5ad11517aeb13
amckean12 <amckean@me.com>
2019-06-18 12:08:19 -0500

    Changed styling for tag and ingredient box

--- src/recipe/components/ingredient-box.js
+++ src/recipe/components/ingredient-box.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 const IngredientBox = (props) => {
   return (
     <div className="display-box">
-      <h1> Create a New Recipe </h1>
+      <h1>Ingredients</h1>
     </div>
   )
 }
--- src/recipe/components/tag-box.js
+++ src/recipe/components/tag-box.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 const TagBox = (props) => {
   return (
     <div className="display-box">
-      <h1> Create a New Recipe </h1>
+      <h1>Tags</h1>
     </div>
   )
 }
