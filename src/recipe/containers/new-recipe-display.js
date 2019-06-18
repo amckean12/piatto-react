769b304286fe1f28785bdf480761a07a85bb6b04
amckean12 <amckean@me.com>
2019-06-18 11:55:23 -0500

    Switch component to right file

--- src/recipe/containers/new-recipe-display.js
+++ src/recipe/containers/new-recipe-display.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import RecipeTitle from '../components/recipe-form-title'
 
 import '../recipe.css'
 
@@ -10,15 +9,7 @@ class NewRecipeDisplay extends Component {
   render(){
     return(
       <div className="new-recipe-display-container">
-        <RecipeTitle />
-        <form>
-          <input type="text" placeholder="Recipe Name"></input>
-          <input type="text" placeholder="Recipe Description"></input>
-        </form>
-        <div className="multi-recipe-inputs">
-          <input type= "text" placeholder="Ingredient"></input>
-          <input type="text" placeholder="Tag"></input>
-        </div>
+
       </div>
     )
   }
--- src/recipe/containers/recipe-form-container.js
+++ src/recipe/containers/recipe-form-container.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom'
 import { } from '../../actions/create-recipe-action.js'
+import RecipeTitle from '../components/recipe-form-title'
 
 import '../recipe.css'
 
 
 class RecipeFormContainer extends Component {
 
+
+
   render(){
     return(
       <div className="recipe-form-container">
-        
+        <RecipeTitle />
+        <form>
+          <input type="text" placeholder="Recipe Name"></input>
+          <input type="text" placeholder="Recipe Description"></input>
+        </form>
+        <div className="multi-recipe-inputs">
+          <input type= "text" placeholder="Ingredient"></input>
+          <input type="text" placeholder="Tag"></input>
+        </div>
       </div>
     )
   }
